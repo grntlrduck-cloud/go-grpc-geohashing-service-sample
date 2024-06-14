586b2d3a9e72b3131bd2273409e3b29d770b4134
grantlerduck <daniel.alker@outlook.de>
2024-06-14 19:08:20 +0200

    Add new lines to test class for better readablility.

--- application/environment_test.go
+++ application/environment_test.go
@@ -12,11 +12,12 @@ var _ = Describe("Test application environment load", func() {
 	When("table name is set", func() {
 		tableName := "some-table"
 		os.Setenv("DYNAMO_TABLE_NAME", tableName)
+		
 		It("does not panic", func() {
 			appEnv := application.NewServiceEnv()
 			Expect(appEnv).Should(Not(BeNil()))
 			Expect(appEnv.TableName).Should(Equal(tableName))
 		})
+	
 	})
-
 })
