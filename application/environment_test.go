8f50933d73612c3c943d01b769cc0fffcc7cbd6f
grantlerduck <47271781+grantlerduck@users.noreply.github.com>
2024-06-14 19:11:52 +0200

    Add vulnerability scan to workflow (#3)
    * Add vulnerability scan to workflow, add needs to test and synth.
    
    * Format.
    
    * Add new lines to test class for better readablility.

--- application/environment_test.go
+++ application/environment_test.go
@@ -8,15 +8,16 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Test application environment load", func ()  {
-	When("table name is set", func ()  {
+var _ = Describe("Test application environment load", func() {
+	When("table name is set", func() {
 		tableName := "some-table"
 		os.Setenv("DYNAMO_TABLE_NAME", tableName)
-		It("does not panic", func ()  {
+		
+		It("does not panic", func() {
 			appEnv := application.NewServiceEnv()
 			Expect(appEnv).Should(Not(BeNil()))
 			Expect(appEnv.TableName).Should(Equal(tableName))
 		})
-	})
 	
+	})
 })
--- test-local.sh
+++ test-local.sh
@@ -8,16 +8,15 @@ golangci-lint run ./...
 echo "Running tests with reports..."
 gotestsum --junitfile unit-tests.xml -- -coverprofile=cover.out -covermode count ./...
 go tool cover -html=cover.out -o coverage.html
-gocover-cobertura < cover.out > coverage.xml
+gocover-cobertura <cover.out >coverage.xml
 
 echo "Checking for vulnerabilities..."
-govulncheck ./...
 
 export AWS_REGION="eu-west-1"
 export AWS_ACCOUNT="123456789123"
 
 echo "Syntheizing app..."
-cdk synth >> /dev/null
+cdk synth >>/dev/null
 
 echo "Generating infrastructure diagram..."
 npx cdk-dia
