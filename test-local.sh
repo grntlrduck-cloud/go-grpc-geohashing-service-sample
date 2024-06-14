cd963c7edbc9bdbaa24b815709067f0d11f24582
grantlerduck <daniel.alker@outlook.de>
2024-06-14 13:42:08 +0200

    Use go bin from setup-go.

--- test-local.sh
+++ test-local.sh
@@ -7,7 +7,7 @@ golangci-lint run ./...
 
 echo "Running tests with reports..."
 gotestsum --junitfile unit-tests.xml -- -coverprofile=cover.out -covermode count ./...
-
+go tool cover -html=cover.out -o coverage.html
 gocover-cobertura < cover.out > coverage.xml
 
 echo "Checking for vulnerabilities..."
