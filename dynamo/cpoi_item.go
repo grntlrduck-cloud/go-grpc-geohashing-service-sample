41ce9c97bf1047c14ec4ce3e93abd769b0c3336f
grantlerduck <daniel.alker@outlook.de>
2024-07-21 14:06:37 +0200

    Flatten structure of struct for easier use in csv import for dynamodb.

--- dynamo/cpoi_item.go
+++ dynamo/cpoi_item.go
@@ -7,21 +7,20 @@ const (
 	CPoIItemGeoIndexSK   = "gsi1_geo_sk"
 )
 
+// The CPoIItem is a flattened representation of the domain with a primary key (hashkey) to get a cPoI by its id 
+// and a global secondary geo index where the primary key (hashkey) is the trimmed geohash and the sortkey is the full precision geohash.
+// The structure is flattend so that a import of the dataset from csv on table creation through IaC is easier and less errorprone.
 type CPoIItem struct {
-	Pk           string   `json:"pk"             dynamodbav:"pk"`
-	GeoIndexPk   uint64   `json:"gsi1_geo_pk_pk" dynamodbav:"gsi1_geo_pk"` // the geohash with trimmed precision
-	GeoIndexSk   uint64   `json:"gsi1_geo_pk"    dynamodbav:"gsi1_geo_sk"` // the geohash with full precision
-	Id           string   `json:"id"             dynamodbav:"id"`
-	WgsPoint        WgsPoint `json:"wgs_point"      dynamodbav:"wgs_point"`
-	Street       string   `json:"street"         dynamodbav:"street"`
-	StreetNumber string   `json:"street_number"  dynamodbav:"street_number"`
-	ZipCode      string   `json:"zip_code"       dynamodbav:"zip_code"`
-	City         string   `json:"city"           dynamodbav:"city"`
-	CountryCode  string   `json:"country_code"   dynamodbav:"country_code"`
-	Features     []string `json:"features"       dynamodbav:"features"`
-}
-
-type WgsPoint struct {
-	Longitude float64 `json:"lon" dynamodbav:"lon"`
-	Latitude  float64 `json:"lat" dynamodbav:"lat"`
+	Pk           string   `json:"pk"             csv:"pk"            dynamodbav:"pk"`
+	GeoIndexPk   uint64   `json:"gsi1_geo_pk_pk" csv:"gsi1_geo_pk"   dynamodbav:"gsi1_geo_pk"` // the geohash with trimmed precision
+	GeoIndexSk   uint64   `json:"gsi1_geo_pk"    csv:"gsi1_geo_sk"   dynamodbav:"gsi1_geo_sk"` // the geohash with full precision
+	Id           string   `json:"id"             csv:"id"            dynamodbav:"id"`
+	Street       string   `json:"street"         csv:"street"        dynamodbav:"street"`
+	StreetNumber string   `json:"street_number"  csv:"street_number" dynamodbav:"street_number"`
+	ZipCode      string   `json:"zip_code"       csv:"zip_code"      dynamodbav:"zip_code"`
+	City         string   `json:"city"           csv:"city"          dynamodbav:"city"`
+	CountryCode  string   `json:"country_code"   csv:"country_code"  dynamodbav:"country_code"`
+	Features     []string `json:"features"       csv:"features"      dynamodbav:"features"`
+	Longitude    float64  `json:"lon"            csv:"lon"           dynamodbav:"lon"`
+	Latitude     float64  `json:"lat"            csv:"lat"           dynamodbav:"lat"`
 }
