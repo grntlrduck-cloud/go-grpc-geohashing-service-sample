fe088a4d536300dc988cb36b0be3cc8399462332
grantlerduck <47271781+grantlerduck@users.noreply.github.com>
2024-07-28 16:45:05 +0200

    feat: processing and upload of sample dataset to s3 (#7)
    * format cpoi item.
    
    * format yaml and json files using prettier.
    
    * feat: add processing of the raw dataset. conversion of the orginial schema to the schema required for the implementation of the service using dynamodb, parsing to csv and aws ion, peresitence of the data on disk and on aws s3.
    
    * chore: use ksuid in domain instead of uuidv4.
    
    * chore: switch to ksuid.

--- cmd/data/main.go
+++ cmd/data/main.go
@@ -7,12 +7,28 @@ import (
 	"log"
 	"os"
 
+	"github.com/amazon-ion/ion-go/ion"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/ssm"
-	_ "github.com/gocarina/gocsv" // TODO use, impelement parsing of csv data to our data model for DynamoDB
+	"github.com/gocarina/gocsv"
+
+	"github.com/grntlduck-cloud/go-grpc-geohasing-service-sample/dynamo"
 )
 
+const (
+	bucketNameParam        = "/config/go-grpc-poi-service/charging-data-bucket-name"
+	cPoIDataCSVPath        = "cpoi_data.csv"
+	cPoIDynamoItemsCSVPath = "cpoi_dynamo_items.csv"
+	cPoIIonFilePath        = "cpoi_ion_items"
+)
+
+// This program requires the dataset from kaggle to be present in the root of this project as 'cpoi_data.csv'.
+// The CSV is proecessed and mapped to fit the data model for dynamo db, saved to disk as CSV and AWS ION.
+// Finally, the files, the raw and the processed data is uploaded to the S3 bucket defined in the data-stack
+// of the infrasturcture.
+// TODO: This program could be optimized by using go flags to conrtol processing, uploads, and file name cusotmization
 func main() {
 	ctx := context.Background()
 	conf, confErr := config.LoadDefaultConfig(ctx)
@@ -22,40 +38,126 @@ func main() {
 
 	log.Print("getting bucket name parameter")
 	ssmClient := ssm.NewFromConfig(conf)
-	bucketNameParam := "/config/go-grpc-poi-service/charging-data-bucket-name"
-	bucketName, getParamErr := paramStr(ctx, ssmClient, bucketNameParam)
-	if getParamErr != nil {
-		panic(fmt.Errorf("failed to get ssm param: %w", getParamErr))
-	}
+	bucketName := paramStr(ctx, ssmClient, bucketNameParam)
 	log.Printf("got bucket name from parameter with value %s", *bucketName)
 
 	// load the csv from file
-	log.Print("loading csv from file...")
-	baseCsv, fileErr := os.Open("cpoi_data.csv")
+	baseCsv := loadFile(cPoIDataCSVPath)
+	defer baseCsv.Close()
+
+	// map entries to struct
+	log.Print("mapping data to dynamoItems")
+	entries := []*dynamo.ChargingCSVEntry{}
+	if csvUnmarshallErr := gocsv.UnmarshalFile(baseCsv, &entries); csvUnmarshallErr != nil {
+		panic(fmt.Errorf("failed to map rows to struct, %w", csvUnmarshallErr))
+	}
+	dynamoItems := dynamo.EntriesToDynamo(entries)
+
+	// write the dyanmoItems as csv to file
+	log.Print("writing dynamo items csv to files")
+	writeCSV(dynamoItems, cPoIDynamoItemsCSVPath)
+
+	log.Print("writing ion file")
+	writeIonFile(dynamoItems, cPoIIonFilePath)
+
+	log.Print("uploading CSVs to s3")
+	// upload the raw data
+	s3Client := s3.NewFromConfig(conf)
+	baseCsvS3 := loadFile(cPoIDataCSVPath)
+	defer baseCsvS3.Close()
+	putObject(ctx, s3Client, aws.String("raw/raw_cpoi_data.csv"), bucketName, baseCsvS3)
+
+	// upload the processed csv
+	dynamoItemsCsvS3 := loadFile(cPoIDynamoItemsCSVPath)
+	defer dynamoItemsCsvS3.Close()
+	putObject(
+		ctx,
+		s3Client,
+		aws.String("dynamo/csv/cpoi_dynamo_items.csv"),
+		bucketName,
+		dynamoItemsCsvS3,
+	)
+
+	// upload the ion items
+	ionFile := loadFile(cPoIIonFilePath)
+	defer ionFile.Close()
+	putObject(
+		ctx,
+		s3Client,
+		aws.String("dynamo/ion/cpoi_ion_items"),
+		bucketName,
+		ionFile,
+	)
+
+	log.Print("Done!")
+}
+
+func writeCSV(items []*dynamo.CPoIItem, filePath string) {
+	itemsFile, createFileErr := os.OpenFile(
+		filePath,
+		os.O_CREATE|os.O_WRONLY,
+		0644,
+	)
+	defer closeFile(itemsFile)
+	if createFileErr != nil {
+		panic(fmt.Errorf("failed to create file for dynamo items, %w", createFileErr))
+	}
+	csvMarshalErr := gocsv.MarshalFile(items, itemsFile)
+	if csvMarshalErr != nil {
+		panic(fmt.Errorf("failed to marshal dynamo items to csv, %w", csvMarshalErr))
+	}
+}
+
+func writeIonFile(items []*dynamo.CPoIItem, filePath string) {
+	ionF, ionFileErr := os.OpenFile(
+		filePath,
+		os.O_CREATE|os.O_WRONLY,
+		0644,
+	)
+	defer closeFile(ionF)
+	if ionFileErr != nil {
+		panic(fmt.Errorf("failed to write ion file, %w", ionFileErr))
+	}
+	writer := ion.NewTextWriter(ionF)
+  defer func(w ion.Writer) {
+    e := w.Finish()
+    if e != nil {
+      panic(fmt.Errorf("failed to close file, %w", e))
+    }
+  }(writer)
+	encoder := ion.NewEncoder(writer)
+	for _, v := range items {
+		ion := v.IonItem()
+		e := encoder.Encode(ion)
+		if e != nil {
+			panic(fmt.Errorf("failed to encode ion itemm, %w", e))
+		}
+	}
+}
+
+func closeFile(f *os.File) {
+  e := f.Close()
+  if e != nil {
+    panic(fmt.Errorf("failed to close file, %w", e))
+  }
+}
+
+func loadFile(filePath string) *os.File {
+	csv, fileErr := os.Open(filePath)
 	if fileErr != nil {
 		panic(fmt.Errorf("failed to load csv from file: %w", fileErr))
 	}
-	defer baseCsv.Close()
-
-	// put base data to s3
-	log.Print("uploading uprocessed csv to s3...")
-	keyRaw := "raw_cpoi_data.csv"
-	s3Client := s3.NewFromConfig(conf)
-	s3PutObjectErr := putObject(ctx, s3Client, &keyRaw, bucketName, baseCsv)
-	if s3PutObjectErr != nil {
-		panic(fmt.Errorf("failed to put csv %s to s3: %w", keyRaw, s3PutObjectErr))
-	}
-	log.Printf("uploaded csv to s3 with key %s", keyRaw)
+	return csv
 }
 
-func paramStr(ctx context.Context, ssmClient *ssm.Client, paramName string) (*string, error) {
+func paramStr(ctx context.Context, ssmClient *ssm.Client, paramName string) *string {
 	param, err := ssmClient.GetParameter(ctx, &ssm.GetParameterInput{
 		Name: &(paramName),
 	})
 	if err != nil {
-		return nil, err
+		panic(fmt.Errorf("faile to get param, %w", err))
 	}
-	return param.Parameter.Value, nil
+	return param.Parameter.Value
 }
 
 func putObject(
@@ -64,14 +166,13 @@ func putObject(
 	key *string,
 	bucket *string,
 	body io.Reader,
-) error {
+) {
 	_, err := s3Client.PutObject(ctx, &s3.PutObjectInput{
 		Bucket: bucket,
 		Key:    key,
 		Body:   body,
 	})
 	if err != nil {
-		return err
+		panic(fmt.Errorf("failed to put object %s to s3, %w", *key, err))
 	}
-	return nil
 }
--- dynamo/cpoi_item.go
+++ dynamo/cpoi_item.go
@@ -1,29 +1,165 @@
 package dynamo
 
-const (
-	CPoIItemPK           = "pk"
-	CPoIItemGeoIndexName = "gsi1_geo"
-	CPoIItemGeoIndexPK   = "gsi1_geo_pk"
-	CPoIItemGeoIndexSK   = "gsi1_geo_sk"
-  CPoIItemGeoHashKeyLength = 4
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/amazon-ion/ion-go/ion"
+	"github.com/segmentio/ksuid"
 )
 
-// The CPoIItem is a flattened representation of the domain with a primary key (hashkey) to get a cPoI by its id 
+const (
+	CPoIItemPK               = "pk"
+	CPoIItemGeoIndexName     = "gsi1_geo"
+	CPoIItemGeoIndexPK       = "gsi1_geo_pk"
+	CPoIItemGeoIndexSK       = "gsi1_geo_sk"
+	CPoIItemGeoHashKeyLength = 4
+	countryCodeDeu           = "DEU"
+	ac                       = "AC"
+	dc                       = "DC"
+)
+
+// The CPoIItem is a flattened representation of the domain with a primary key (hashkey) to get a cPoI by its id
 // and a global secondary geo index where the primary key (hashkey) is the trimmed geohash and the sortkey is the full precision geohash.
 // The structure is flattend so that a import of the dataset from csv on table creation through IaC is easier and less errorprone.
 type CPoIItem struct {
-	Pk           string   `json:"pk"             csv:"pk"            dynamodbav:"pk"`
-	GeoIndexPk   uint64   `json:"gsi1_geo_pk_pk" csv:"gsi1_geo_pk"   dynamodbav:"gsi1_geo_pk"` // the geohash with trimmed precision
-	GeoIndexSk   uint64   `json:"gsi1_geo_pk"    csv:"gsi1_geo_sk"   dynamodbav:"gsi1_geo_sk"` // the geohash with full precision
-	Id           string   `json:"id"             csv:"id"            dynamodbav:"id"`
-	Street       string   `json:"street"         csv:"street"        dynamodbav:"street"`
-	StreetNumber string   `json:"street_number"  csv:"street_number" dynamodbav:"street_number"`
-	ZipCode      string   `json:"zip_code"       csv:"zip_code"      dynamodbav:"zip_code"`
-	City         string   `json:"city"           csv:"city"          dynamodbav:"city"`
-	CountryCode  string   `json:"country_code"   csv:"country_code"  dynamodbav:"country_code"`
-	Features     []string `json:"features"       csv:"features"      dynamodbav:"features"`
-	Longitude    float64  `json:"lon"            csv:"lon"           dynamodbav:"lon"`
-	Latitude     float64  `json:"lat"            csv:"lat"           dynamodbav:"lat"`
-	EntranceLongitude    float64  `json:"entrance_lon"            csv:"entrance_lon"           dynamodbav:"entrance_lon"`
-	EntranceLatitude     float64  `json:"entrance_lat"            csv:"entrance_lat"           dynamodbav:"entrance_lat"`
+	Pk                string   `json:"pk"             csv:"pk"            dynamodbav:"pk"`
+	GeoIndexPk        uint64   `json:"gsi1_geo_pk_pk" csv:"gsi1_geo_pk"   dynamodbav:"gsi1_geo_pk"` // the geohash with trimmed precision
+	GeoIndexSk        uint64   `json:"gsi1_geo_pk"    csv:"gsi1_geo_sk"   dynamodbav:"gsi1_geo_sk"` // the geohash with full precision
+	Id                string   `json:"id"             csv:"id"            dynamodbav:"id"`
+	Street            string   `json:"street"         csv:"street"        dynamodbav:"street"`
+	StreetNumber      string   `json:"street_number"  csv:"street_number" dynamodbav:"street_number"`
+	ZipCode           string   `json:"zip_code"       csv:"zip_code"      dynamodbav:"zip_code"`
+	City              string   `json:"city"           csv:"city"          dynamodbav:"city"`
+	CountryCode       string   `json:"country_code"   csv:"country_code"  dynamodbav:"country_code"`
+	Features          []string `json:"features"       csv:"features"      dynamodbav:"features"`
+  Longitude         float64  `json:"lon"            csv:"lon"           dynamodbav:"lon"`
+  Latitude          float64  `json:"lat"            csv:"lat"           dynamodbav:"lat"`
+  EntranceLongitude float64  `json:"entrance_lon"   csv:"entrance_lon"  dynamodbav:"entrance_lon"`
+  EntranceLatitude  float64  `json:"entrance_lat"   csv:"entrance_lat"  dynamodbav:"entrance_lat"`
+}
+
+func (cp CPoIItem) IonItem() *IonItem {
+  return &IonItem{
+		CPoIIonItem{
+			Pk:                cp.Pk,
+			GeoIndexPk:        *ion.NewDecimalInt(int64(cp.GeoIndexPk)),
+			GeoIndexSk:        *ion.NewDecimalInt(int64(cp.GeoIndexSk)),
+			Id:                cp.Id,
+			Street:            cp.Street,
+			StreetNumber:      cp.StreetNumber,
+			ZipCode:           cp.ZipCode,
+			City:              cp.City,
+			CountryCode:       cp.CountryCode,
+			Features:          cp.Features,
+			Longitude:         floatToDecimal(cp.Longitude),
+			Latitude:          floatToDecimal(cp.Latitude),
+			EntranceLongitude: floatToDecimal(cp.EntranceLongitude),
+			EntranceLatitude:  floatToDecimal(cp.EntranceLatitude),
+		},
+	}
+}
+
+func floatToDecimal(fnum float64) ion.Decimal {
+  fstr := strconv.FormatFloat(fnum, 'f', -1, 64)
+	n, e := ion.ParseDecimal(fstr)
+  if e != nil {
+    panic(fmt.Errorf("failed to convert float %s, %w", fstr, e))
+  }
+	return *n
+}
+
+// The struct to model the data contained in the dataset, see https://www.kaggle.com/datasets/mexwell/electric-vehicle-charging-in-germany
+type ChargingCSVEntry struct {
+	ChargingType         string  `csv:"art_der_ladeeinrichtung"`
+	Power                float32 `csv:"anschlussleistung"`
+	NumberOfChargePoints int8    `csv:"anzahl_ladepunkte"`
+	PlugType1            string  `csv:"steckertypen1"`
+	PlugType2            string  `csv:"steckertypen2"`
+	PlugType3            string  `csv:"steckertypen3"`
+	PlugType4            string  `csv:"steckertypen4"`
+	City                 string  `csv:"ort"`
+	ZipCode              string  `csv:"postleitzahl"`
+	Street               string  `csv:"strasse"`
+	StreetNumber         string  `csv:"hausnummer"`
+	Longitude            float64 `csv:"laengengrad"`
+	Latitude             float64 `csv:"breitengrad"`
+}
+
+func EntriesToDynamo(ctes []*ChargingCSVEntry) []*CPoIItem {
+	dynamoItems := make([]*CPoIItem, len(ctes))
+	for i, cte := range ctes {
+		dynamoItems[i] = cte.MapToDynamo()
+	}
+	return dynamoItems
+}
+
+func (cte *ChargingCSVEntry) MapToDynamo() *CPoIItem {
+	gh := NewGeoHash(cte.Latitude, cte.Longitude)
+	id := ksuid.New().String()
+	return &CPoIItem{
+		Pk: id,
+		GeoIndexPk: gh.Trimmed(
+			CPoIItemGeoHashKeyLength,
+		), // the trimmed geo hash representing a tile
+		GeoIndexSk:        gh.Hash(), // the full length geo hash
+		Id:                id,
+		Street:            cte.Street,
+		StreetNumber:      cte.StreetNumber,
+		ZipCode:           cte.ZipCode,
+		City:              cte.City,
+		CountryCode:       countryCodeDeu,
+		Longitude:         cte.Longitude,
+		Latitude:          cte.Latitude,
+		EntranceLongitude: cte.Longitude,
+		EntranceLatitude:  cte.Latitude,
+		Features:          cte.features(),
+	}
+}
+
+func (cte *ChargingCSVEntry) features() []string {
+	features := make([]string, 2)
+	features[0] = fmt.Sprintf("%d_CHARGEPOINTS", cte.NumberOfChargePoints)
+	features[1] = fmt.Sprintf("%d_KW_CHARGING", int32(cte.Power))
+	if cte.hasAcCharging() {
+		features = append(features, "AC_CHAGRING")
+	}
+	if cte.hasDcCharging() {
+		features = append(features, "DC_CHARGING")
+	}
+	return features
+}
+
+func (cte *ChargingCSVEntry) hasAcCharging() bool {
+	return strings.Contains(cte.PlugType1, ac) || strings.Contains(cte.PlugType2, ac) ||
+		strings.Contains(cte.PlugType3, ac) ||
+		strings.Contains(cte.PlugType4, ac)
+}
+
+func (cte *ChargingCSVEntry) hasDcCharging() bool {
+	return strings.Contains(cte.PlugType1, dc) || strings.Contains(cte.PlugType2, dc) ||
+		strings.Contains(cte.PlugType3, dc) ||
+		strings.Contains(cte.PlugType4, dc)
+}
+
+type IonItem struct {
+	Item CPoIIonItem `ion:"Item"`
+}
+
+type CPoIIonItem struct {
+	Pk                string      `ion:"pk"`
+	GeoIndexPk        ion.Decimal `ion:"gsi1_geo_pk"`
+	GeoIndexSk        ion.Decimal `ion:"gsi1_geo_sk"`
+	Id                string      `ion:"id"`
+	Street            string      `ion:"street"`
+	StreetNumber      string      `ion:"street_number"`
+	ZipCode           string      `ion:"zip_code"`
+	City              string      `ion:"city"`
+	CountryCode       string      `ion:"country_code"`
+	Features          []string    `ion:"features"`
+	Longitude         ion.Decimal `ion:"lon"`
+	Latitude          ion.Decimal `ion:"lat"`
+	EntranceLongitude ion.Decimal `ion:"entrance_lon"`
+	EntranceLatitude  ion.Decimal `ion:"entrance_lat"`
 }
