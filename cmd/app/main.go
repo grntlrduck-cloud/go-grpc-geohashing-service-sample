fbb18582c98c5e3aebe3793ea735c42283b6594a
grantlerduck <daniel.alker@outlook.de>
2024-08-18 18:46:12 +0200

    chore: fix typo

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -41,7 +41,7 @@ func main() {
 	c := make(chan os.Signal, 2)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 
-	logger.Info("runnering and serving requests")
+	logger.Info("running and serving requests")
 	for {
 		select {
 		case <-c:
